91efc70a8d277fe9d4ab08a9a56ee8f4a581183e
ericyork <eyork@clarkson.edu>
2023-01-15 08:07:39 -0500

    adds support for quote fetching three ways

---
+++ js/data.js
@@ -0,0 +1,202 @@
+const quotesArr = [
+  {
+    "text": "A beautiful thing is never perfect.",
+    "author": "Anonymous",
+    "category": "sappy"
+  },
+  {
+    "text": "A house divided against itself cannot stand.",
+    "author": "Abraham Lincoln",
+    "category": "historical"
+  },
+  {
+    "text": "Doing nothing is better than being busy doing nothing.",
+    "author": "Lao Tzu",
+    "category": "proverb"
+  },
+  {
+    "text": "Well begun is half done.",
+    "author": "Aristotle",
+    "category": "philosophy"
+  },
+  {
+    "text": "He is able who thinks he is able.",
+    "author": "Buddha",
+    "category": "philosophy"
+  },
+  {
+    "text": "Difficulties increase the nearer we get to the goal.",
+    "author": "Johann Wolfgang von Goethe",
+    "category": "philosophy"
+  },
+  {
+    "text": "Life is a learning experience, only if you learn.",
+    "author": "Yogi Berra",
+    "category": "historical"
+  },
+  {
+    "text": "Talk doesn't cook rice.",
+    "author": "Chinese proverb",
+    "category": "proverb"
+  },
+  {
+    "text": "Change in all things is sweet.",
+    "author": "Aristotle",
+    "category": "philosophy"
+  },
+  {
+    "text": "Victory belongs to the most persevering.",
+    "author": "Napoleon Bonaparte",
+    "category": "historical"
+  },
+  {
+    "text": "The day is already blessed, find peace within it.",
+    "author": "Anonymous",
+    "category": "sappy"
+  },
+  {
+    "text": "Love all, trust a few, do wrong to none.",
+    "author": "William Shakespeare",
+    "category": "historical"
+  },
+  {
+    "text": "Do, or do not. There is no try.",
+    "author": "Yoda",
+    "category": "historical"
+  },
+  {
+    "text": "From small beginnings come great things.",
+    "author": "Anonymous",
+    "category": "sappy"
+  },
+  {
+    "text": "The two most powerful warriors are patience and time.",
+    "author": "Leo Tolstoy",
+    "category": "philosophy"
+  },
+  {
+    "text": "When deeds speak, words are nothing.",
+    "author": "Traditional African Saying",
+    "category": "proverb"
+  },
+  {
+    "text": "Learn from yesterday, live for today, hope for tomorrow.",
+    "author": "Albert Einstein",
+    "category": "philosophy"
+  },
+  {
+    "text": "Think how hard physics would be if particles could think.",
+    "author": "Murray Gell-Mann",
+    "category": "philosophy"
+  },
+  {
+    "text": "It takes both sunshine and rain to make a rainbow.",
+    "author": "Anonymous",
+    "category": "sappy"
+  },
+  {
+    "text": "Those who are free of resentful thoughts surely find peace.",
+    "author": "Buddha",
+    "category": "proverb"
+  },
+  {
+    "text": "All serious daring starts from within.",
+    "author": "Harriet Beecher Stowe",
+    "category": "historical"
+  },
+  {
+    "text": "We can only learn to love by loving.",
+    "author": "Iris Murdoch",
+    "category": "sappy"
+  },
+  {
+    "text": "I never think of the future. It comes soon enough.",
+    "author": "Albert Einstein",
+    "category": "philosophy"
+  },
+  {
+    "text": "The weak can never forgive. Forgiveness is the attribute of the strong.",
+    "author": "Mohandas Gandhi",
+    "category": "philosophy"
+  },
+  {
+    "text": "Even a blade of grass... becomes a mysterious, awesome, indescribably magnificent world in itself.",
+    "author": "Henry Miller",
+    "category": "philosophy"
+  },
+  {
+    "text": "The best thing about the future is that it only comes one day at a time.",
+    "author": "Abraham Lincoln",
+    "category": "historical"
+  },
+  {
+    "text": "Self-trust is the first secret of success.",
+    "author": "Ralph Waldo Emerson",
+    "category": "philosophy"
+  },
+  {
+    "text": "If I ever get real rich, I hope I’m not real mean to poor people, like I am now.",
+    "author": "Jack Handy",
+    "category": "silly"
+  },
+  {
+    "text": "To me, boxing is like a ballet, except there’s no music, no choreography, and the dancers hit each other.",
+    "author": "Jack Handy",
+    "category": "silly"
+  },
+  {
+    "text": "I can picture in my mind a world without war, a world without hate. And I can picture us attacking that world, because they’d never expect it",
+    "author": "Jack Handy",
+    "category": "silly"
+  },
+  {
+    "text": "To be wronged is nothing unless you continue to remember it.",
+    "author": "Confucious",
+    "category": "philosophy"
+  },
+  {
+    "text": "A thing long expected takes the form of the unexpected when at last it comes.",
+    "author": "Mark Twain",
+    "category": "historical"
+  },
+  {
+    "text": "Loss is nothing else but change, and change is nature's delight",
+    "author": "Marcus Aurelius",
+    "category": "historical"
+  },
+  {
+    "text": "He is a wise man who does not grieve for the things which he has not, but rejoices for those which he has.",
+    "author": "Epictetus",
+    "category": "philosophy"
+  },
+  {
+    "text": "Take heed: you do not find what you do not seek.",
+    "author": "English proverb",
+    "category": "proverb"
+  },
+  {
+    "text": "The day always looks brighter from behind a smile.",
+    "author": "Anonymous",
+    "category": "sappy"
+  },
+  {
+    "text": "The more you know yourself, the more you forgive yourself.",
+    "author": "Confucius",
+    "category": "philosophy"
+  },
+  {
+    "text": "Iron rusts from disuse; water loses its purity from stagnation... even so does inaction sap the vigour of the mind.",
+    "author": "Leonardo da Vinci",
+    "category": "historical"
+  },
+  {
+    "text": "To understand the heart and mind of a person, look not at what he has already achieved, but at what he aspires to do.",
+    "author": "Kahlil Gibran",
+    "category": "philosophy"
+  },
+  {
+    "text": "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+    "author": "Aristotle",
+    "category": "philosophy"
+  }
+]
--- js/main.js
+++ js/main.js
@@ -1,7 +1,50 @@
-window.onload = () => {
-  const test = document.getElementById('test');
-  test.innerHTML = '<p aria-busy="true">JavaScript is loaded</p>';
-  test.addEventListener("click", () => {
-  test.innerHTML = '';
-  })
-  }
+// gets quotes from the local array
+function getCat() {
+  let selected = document.getElementById('select').value;
+  let quoteText = document.getElementById('blockquote');
+  let quoteAuthor = document.getElementById('author');
+  let cat = quotesArr.filter(quote => quote.category === selected);
+  let rand = Math.floor(Math.random() * cat.length);
+  quoteText.innerText = cat[rand].text;
+    if (cat[rand].author === null) {
+      quoteAuthor.innerText = "Anonymous";
+    } else {
+      quoteAuthor.innerText = cat[rand].author;
+    };
+  console.log(cat[rand]);
+}
+// gets quotes (without category) from a public API call
+function showOne() {
+  let rand = Math.floor(Math.random() * 999);
+  let quoteText = document.getElementById('blockquote');
+  let quoteAuthor = document.getElementById('author');
+  // fetch.then promise syntax
+  fetch('https://type.fit/api/quotes')
+  .then(res => res.json())
+  .then(data => {
+    quoteText.innerText = data[rand].text;
+    if (data[rand].author === null) {
+      quoteAuthor.innerText = "Anonymous";
+    } else {
+      quoteAuthor.innerText = data[rand].author;
+    }
+    console.log(data[rand]);
+  });
+}
+
+// gets category-less quotes from the API using alternative syntax
+// async function showOne() {
+//   let rand = Math.floor(Math.random() * 99);
+//   let quoteText = document.getElementById('blockquote');
+//   let quoteAuthor = document.getElementById('author');
+//   // alternate promise syntax
+//   let response = await fetch('https://type.fit/api/quotes');
+//   let data = await response.json();
+//   quoteText.innerText = data[rand].text;
+//   if (data[rand].author === null) {
+//     quoteAuthor.innerText = "Anonymous";
+//   } else {
+//     quoteAuthor.innerText = data[rand].author;
+//   }
+//   console.log(data[rand]);
+// }
